da921bdb6adb9454c1c9ff03660be3785009091c
Sandhya80 <129001012+Sandhya80@users.noreply.github.com>
2025-07-04 02:59:37 +0100

    made essential configurations

--- deal_radar/settings.py
+++ deal_radar/settings.py
@@ -99,10 +99,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
+
+# For production later
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files
 MEDIA_URL = '/media/'
