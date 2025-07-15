267d4f0a6714cf1ca8a5b043023ca9649dcdfbe0
Sandhya80 <129001012+Sandhya80@users.noreply.github.com>
2025-07-15 14:32:29 +0100

    Fix: import require_POST for Stripe billing portal

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
